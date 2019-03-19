9f4919bbc8e39969ad0b2b54195c38bcc0a56df5
Seth Musulin <smusulin@LAMU02PP0CWG3QN.uhc.com>
2019-03-19 12:46:09 -0600

    Just show the JSON to start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import {appointments} from './data/appointments'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Put some Code in here
+          <pre>{JSON.stringify(appointments, null, '  ')}</pre>
         </header>
       </div>
     );
